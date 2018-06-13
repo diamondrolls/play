98763a7a2d50f59c409130a0d75e6a3cd066f22b
sthomp18 <33614697+sthomp18@users.noreply.github.com>
2018-06-13 02:57:07 -0500

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,5 +1,11 @@
 <?php
 
-funtion setComments() {
+funtion setComments($conn) {
   if (isset($_POST[commentSubmit])){
-}
+   $uid= $_POST['uid'];
+     $date= $_POST['date'];
+     $message= $_POST['messege'];
+    $sql= "INSERT INTO comments (uid, date, message)VALUES('$uid', '$date', '$message')";
+    $result= mysqli_query($conn,$sqli);
+  }
+  }
